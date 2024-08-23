6f259f7c7e12f6c56492490714b1be230193d3a0
DenizOttoo <denizotto@hotmail.com>
2024-08-23 21:05:50 +0300

    Add files via upload

---
+++ OkulYonetimUygulamasiG040-Temel/OkulYonetimUygulmasi/OkulYonetimUygulmasi/Ogrenci.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkulYonetimUygulmasi
+{
+    internal class Ogrenci
+    {
+        public int No { get; set; }
+        public string Ad{ get; set; }
+        public string Soyad{ get; set; }
+        public DateTime DogumTarihi{ get; set; }
+        public float Ortalama{ get; set; }
+        public SUBE Sube{ get; set; }
+        public CINSIYET Cinsiyet{ get; set; }
+        public Adres Adresi{ get; set; }
+
+
+        public List<DersNotu> Notlar = new List<DersNotu>();
+        public List<string> Kitaplar = new List<string>();
+
+    }
+
+    public enum SUBE
+    {
+        Empty, A,B,C
+    }
+
+    public enum CINSIYET
+    {
+        Empty, Kiz,Erkek
+    }
+
+}
---
+++ OkulYonetimUygulamasiG040-Temel/OkulYonetimUygulmasi/OkulYonetimUygulmasi/Okul.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OkulYonetimUygulmasi
+{
+    internal class Okul
+    {
+        // Okula ait herhangi bir bilginin değiştirilme işlemleri bu sınıfta yapılmalı
+        public List<Ogrenci> Ogrenciler = new List<Ogrenci>();
+
+        public void OgrenciEkle(int no, string ad, string soyad, DateTime dogumTarihi, CINSIYET cinsiyet, SUBE sube)
+        {
+            Ogrenci o = new Ogrenci();
+            o.Ad = ad;
+            o.No = no;
+
+            // 
+            //
+
+            this.Ogrenciler.Add(o);
+        }
+
+
+        public void NotEkle(int no, string ders,int not )
+        {
+            Ogrenci o = this.Ogrenciler.Where(a => a.No == no).FirstOrDefault();
+
+            if (o!=null)
+            {
+                o.Notlar.Add(new DersNotu(ders, not));
+            }
+        }
+
+    }
+}
